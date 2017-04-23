b68785d568962513e6fce562b2208b624e3fa1f2
Rudolph Hernandez <rudolpharthur@gmail.com>
2017-04-22 17:05:32 -0700

    finished and added snippet for localhost

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 
 #### Example usage:
 * [base_url]/api/whoami
+* https://opalescent-accelerator.glitch.me/api/whoami  
 
 #### Example output:
 * `{"ipaddress":"159.20.14.100","language":"en-US,en;q=0.5","software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}`
--- server.js
+++ server.js
@@ -5,8 +5,11 @@
 var express = require('express');
 var app = express();
 
+var dotenv = require('dotenv');
+dotenv.load();
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,9 +22,18 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
+});
+
+app.get('/api/whoami', function(req, res) {
+  var notLocal = req.headers['x-forwarded-for'];
+  res.json({
+    ipaddress: notLocal ? req.headers['x-forwarded-for'].split(',')[0] : req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
 
